32816bb6101b089fbb6d53440e4900189e561946
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2010-03-27 13:33:15 +0000

    Import of MIYAGAWA/Devel-StackTrace-AsHTML-0.07 from CPAN.
    gitpan-cpan-distribution: Devel-StackTrace-AsHTML
    gitpan-cpan-version:      0.07
    gitpan-cpan-path:         MIYAGAWA/Devel-StackTrace-AsHTML-0.07.tar.gz
    gitpan-cpan-author:       MIYAGAWA
    gitpan-cpan-maturity:     released

--- lib/Devel/StackTrace/AsHTML.pm
+++ lib/Devel/StackTrace/AsHTML.pm
@@ -2,7 +2,7 @@ package Devel::StackTrace::AsHTML;
 
 use strict;
 use 5.008_001;
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 use Data::Dumper;
 use Devel::StackTrace;
@@ -101,10 +101,6 @@ HEAD
         );
     }
     $out .= qq{</ol>};
-
-    $out .= qq{<p class="message">Install <a target="_blank" href="http://search.cpan.org/perldoc?Devel::StackTrace::WithLexicals">Devel::StackTrace::WithLexicals</a> to see the lexical variables of each frame.</p>}
-        unless $trace->frame(0)->can('lexicals');
-
     $out .= "</body></html>";
 
     $out;
